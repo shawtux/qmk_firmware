94d0939cc0085b5db5875452a50fcc16badc13da
lalalademaxiya1 <2831039915@qq.com>
2023-06-30 11:48:42 +0800

    Updated q6 pro

--- keyboards/keychron/q6_pro/iso_encoder/config.h
+++ keyboards/keychron/q6_pro/iso_encoder/config.h
@@ -39,9 +39,7 @@
 #    define CAPS_LOCK_INDEX 61
 #    define LOW_BAT_IND_INDEX 99
 
-// RGB Matrix Animation modes. Explicitly enabled
-// For full list of effects, see:
-// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+/* Enable Reactive Animation */
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGB_MATRIX_KEYPRESSES
 
