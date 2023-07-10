70114051a855ccddda06dc0ece0ecab1ba03a3b2
lokher <lokher@gmail.com>
2023-07-10 15:54:26 +0800

    Update keyboards/keychron/k1_pro/iso/rgb/config.h

--- keyboards/keychron/k1_pro/iso/rgb/config.h
+++ keyboards/keychron/k1_pro/iso/rgb/config.h
@@ -39,9 +39,6 @@
 /* Indication led */
 #    define LOW_BAT_IND_INDEX 80
 
-// RGB Matrix Animation modes. Explicitly enabled
-// For full list of effects, see:
-// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
 #    define RGB_MATRIX_KEYPRESSES
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
